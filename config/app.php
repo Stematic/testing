0167d190825f2b4610f9f771c03b8ddb78f66519
Paul Dawson <me@pauldawson.me>
2023-03-04 14:59:14 +0000

    update version, force copying config file

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | number MAJOR.MINOR.PATCH when an update happens: https://semver.org.
     |
     */
-    'version' => '2.0.0',
+    'version' => '2.0.1',
 
     /*
     |--------------------------------------------------------------------------
